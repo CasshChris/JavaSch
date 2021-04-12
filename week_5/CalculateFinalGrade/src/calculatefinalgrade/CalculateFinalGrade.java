103cc7a088a51b5514f4d835d90ba720bc17f2d1
Christian Alvarez <alvarez.christian46@outlook.com>
2021-04-12 07:05:17 -0500

    week 5 redo

--- week_5/CalculateFinalGrade/src/calculatefinalgrade/CalculateFinalGrade.java
+++ week_5/CalculateFinalGrade/src/calculatefinalgrade/CalculateFinalGrade.java
@@ -7,6 +7,8 @@ package calculatefinalgrade;
 // i :: import Scanner
 import java.util.Scanner;
 
+import jdk.javadoc.internal.doclets.formats.html.resources.standard;
+
 public class CalculateFinalGrade {
     /*Global*/
     // 1 :: Create global "input" object
@@ -22,6 +24,7 @@ public class CalculateFinalGrade {
     public static int aCounter = 0;
 
     public static void main(String[] args) {
+        /*Main*/
         // Main :: print heading
         printHeadings();
         
@@ -30,9 +33,38 @@ public class CalculateFinalGrade {
             // Main.2.2 :: prompt user for input
             promptUserForInputData();
 
-            // Main.3 :: Call findAverageAndStatus() method
+            // Main.2.3 :: Call findAverageAndStatus() method
             findAverageAndStatus();
+
+            // Main.2.4 :: Increase counters +1 & add total average
+            studentCounter++;
+            totalAverage += average;
+            aCounter++;
         }
+
+        // Main.3 :: Call footers
+        printFootings();
+
+    }
+
+    public static void printHeadings() {
+        // 1 :: print header
+        System.out.println("************************************************");
+        System.out.println("***** Calculate Average of 2 Tests Project *****");
+        System.out.println("************************************************");
+        System.out.println();    // print a Blank Line
+    }
+
+    public static void promptUserForInputData() {
+        // 1 :: prompt user for full name
+        System.out.print("Enter your Full Name: ");
+        fullName = input.nextLine();
+
+        // 2 :: prompt user for test 1 & store it
+        System.out.print("Please enter test 1( Between 0 - 100): ");
+        test1 = input.nextInt();
+
+        // 3 :: 
     }
         
 }
